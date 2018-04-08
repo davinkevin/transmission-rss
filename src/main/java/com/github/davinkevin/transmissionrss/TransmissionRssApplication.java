eba0147526a2cebd36703c1b0c3d3d7986702bdf
davinkevin <davin.kevin@gmail.com>
2018-04-08 11:13:41 +0200

    chore(init): initial commit from spring initializer

---
+++ src/main/java/com/github/davinkevin/transmissionrss/TransmissionRssApplication.java
@@ -0,0 +1,12 @@
+package com.github.davinkevin.transmissionrss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransmissionRssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransmissionRssApplication.class, args);
+    }
+}

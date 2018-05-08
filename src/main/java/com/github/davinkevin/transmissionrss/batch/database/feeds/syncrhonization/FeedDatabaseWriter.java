e35ba822d7685739c74a619fd64f6fb1c8218599
davinkevin <davin.kevin@gmail.com>
2018-05-08 15:35:49 +0200

    feat(pattern-matcher): use batch syntax instead of multiple insert

--- src/main/java/com/github/davinkevin/transmissionrss/batch/database/feeds/syncrhonization/FeedDatabaseWriter.java
+++ src/main/java/com/github/davinkevin/transmissionrss/batch/database/feeds/syncrhonization/FeedDatabaseWriter.java
@@ -1,10 +1,12 @@
 package com.github.davinkevin.transmissionrss.batch.database.feeds.syncrhonization;
 
 import com.github.davinkevin.transmissionrss.batch.database.items.syncronization.model.ItemFetchingSpecification;
+import com.github.davinkevin.transmissionrss.tables.records.PatternMatcherRecord;
 import io.vavr.collection.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jooq.DSLContext;
+import org.jooq.InsertValuesStep4;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
@@ -25,10 +27,13 @@ public class FeedDatabaseWriter implements ItemWriter<ItemFetchingSpecification>
 
         query.deleteFrom(PATTERN_MATCHER).execute();
 
-        List.ofAll(items).foldLeft(
-                this.query.insertInto(PATTERN_MATCHER, PATTERN_MATCHER.URL, PATTERN_MATCHER.MATCHER, PATTERN_MATCHER.EXCLUDE, PATTERN_MATCHER.DOWNLOAD_PATH),
-                (v, p) -> v.values(p.getUrl().toString(), p.getMatcher(), p.getExclude(), p.getDownloadPath())
-        ).execute();
+        InsertValuesStep4<PatternMatcherRecord, String, String, String, String> insertPM = query
+                .insertInto(PATTERN_MATCHER, PATTERN_MATCHER.URL, PATTERN_MATCHER.MATCHER, PATTERN_MATCHER.EXCLUDE, PATTERN_MATCHER.DOWNLOAD_PATH);
+
+        List<InsertValuesStep4<PatternMatcherRecord, String, String, String, String>> inserts = List.ofAll(items)
+                .map(p -> insertPM.values(p.getUrl().toString(), p.getMatcher(), p.getExclude(), p.getDownloadPath()));
+
+        query.batch(inserts.toJavaList()).execute();
     }
 
 }
